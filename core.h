1f35063430cef6ade7db02092d30eddc5cd57cd3
papernutcracker <papernutcracker@gmail.com>
2026-03-20 18:30:46 +0200

    Create core.h

---
+++ core.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include "models.h"
+
+// Менеджер валют (Singleton)
+class CurrencyManager {
+private:
+    std::unordered_map<std::string, double> exchangeRates;
+    std::string baseCurrency;
+    CurrencyManager();
+public:
+    static CurrencyManager& getInstance();
+    CurrencyManager(const CurrencyManager&) = delete;
+    CurrencyManager& operator=(const CurrencyManager&) = delete;
+    double convert(double amount, const std::string& from, const std::string& to);
+};
+
+// Менеджер рахунків
+class AccountManager {
+private:
+    std::vector<std::shared_ptr<Account>> accounts;
+    int transactionCounter = 1;
+    int accountCounter = 1;
+    std::string generateTxId();
+public:
+    std::string generateAccId();
+    void setCounters(int accCount, int txCount);
+    void addAccount(std::shared_ptr<Account> account);
+    std::vector<std::shared_ptr<Account>> getAccounts() const;
+    std::shared_ptr<Account> getAccountById(const std::string& id);
+    bool makeExpense(const std::string& accountId, double amount, const std::string& category, const std::string& description, std::string date, std::string userName);
+    bool makeIncome(const std::string& accountId, double amount, const std::string& category, const std::string& description, std::string date, std::string userName);
+    std::vector<Transaction> getTransactionsForUser(const std::string& userName) const;
+};
+
+// Робота з файлами
+class StorageManager {
+public:
+    static void saveToFile(const AccountManager& manager, const std::string& filename);
+    static void loadFromFile(AccountManager& manager, const std::string& filename);
+};
+
+// Звіти
+class ReportGenerator {
+public:
+    static std::vector<Transaction> getTop3Expenses(const std::vector<Transaction>& history, const std::string& startDate, const std::string& endDate);
+    static std::map<std::string, double> getExpensesByUser(const std::vector<Transaction>& history, const std::string& startDate, const std::string& endDate);
+};
